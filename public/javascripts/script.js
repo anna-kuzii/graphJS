94222cbf8315f9109e195977b409bbbb1da24fc1
Anna Kuzii <anna_kuzij@ukr.net>
2017-12-26 01:08:53 +0200

    add layout

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -78,23 +78,29 @@ function hasPathTo(v) {
     return this.marked[v];
 }
 
-g = new Graph("CS1, CS2, Data Structures, Assembly Language, Operating Systems, Algorithms");
+function submitGraph() {
+    var enteredGraph = document.getElementsByClassName('vertex-list')[0].value;
 
-g.showGraph();
-g.addEdge(0,1);
-g.addEdge(0,2);
-g.addEdge(1,3);
-g.addEdge(2,4);
-g.bfs(0);
-
-
-var vertex = 4;
-var paths = g.pathTo(vertex);
-while (paths.length > 0) {
-    if (paths.length > 1) {
-        console.log(paths.pop() + '-');
-    }
-    else {
-        console.log(paths.pop());
-    }
+    var g = new Graph(enteredGraph);
+    g.showGraph();
 }
+
+
+
+// g.addEdge(0,1);
+// g.addEdge(0,2);
+// g.addEdge(1,3);
+// g.addEdge(2,4);
+// g.bfs(0);
+
+
+// var vertex = 4;
+// var paths = g.pathTo(vertex);
+// while (paths.length > 0) {
+//     if (paths.length > 1) {
+//         console.log(paths.pop() + '-');
+//     }
+//     else {
+//         console.log(paths.pop());
+//     }
+// }
